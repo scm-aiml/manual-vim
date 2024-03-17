5c2afd3595ffc9dffd36a1ff88e70094a5098ca0
scm <scm>
2024-03-17 08:04:11 -0400

    telescope

--- clean-and-install.sh
+++ clean-and-install.sh
@@ -23,8 +23,7 @@ cp -r nvim_base/* ~/.config/nvim
 # Everforest theme
 echo "Setting up: Theme Everforest"
 mkdir -p ~/.config/nvim/pack/themes/start
-cd ~/.config/nvim/pack/themes/start
-git clone --quiet https://github.com/sainnhe/everforest.git
+git clone --quiet https://github.com/sainnhe/everforest.git ~/.config/nvim/pack/themes/start/everforest.git
 
 
 # Todo-higlight
@@ -45,14 +44,30 @@ git clone --quiet https://github.com/numToStr/Comment.nvim ~/.config/nvim/pack/p
 echo "Setting up: Gitsigns"
 git clone --quiet https://github.com/lewis6991/gitsigns.nvim.git ~/.config/nvim/pack/plugins/start/gitsigns.nvim
 
-# Gitsigns
+# whichkey
 echo "Setting up: folke Whichkey"
 git clone --quiet https://github.com/folke/which-key.nvim.git ~/.config/nvim/pack/plugins/start/which-key.nvim
 
 
-# Gitsigns
+# conform
 echo "Setting up: autoformat"
 git clone --quiet https://github.com/stevearc/conform.nvim.git ~/.config/nvim/pack/plugins/start/conform.nvim
+
+# telescope
+echo "Setting up: telescope"
+git clone --quiet --branch 0.1.x https://github.com/nvim-telescope/telescope.nvim.git ~/.config/nvim/pack/plugins/start/telescope.nvim
+if command -v make &> /dev/null; then
+    git clone --quiet https://github.com/nvim-telescope/telescope-fzf-native.nvim.git ~/.config/nvim/pack/plugins/start/telescope-fzf-native.nvim
+    cd ~/.config/nvim/pack/plugins/start/telescope-fzf-native.nvim
+    echo "Make telescope-fzf-native"
+    make
+    cd ~/Documents/repo/nvim-manual
+fi
+git clone --quiet https://github.com/nvim-telescope/telescope-ui-select.nvim.git ~/.config/nvim/pack/plugins/start/telescope-ui-select.nvim
+# If you have a Nerd Font and want icons
+git clone --quiet https://github.com/nvim-tree/nvim-web-devicons.git ~/.config/nvim/pack/plugins/start/nvim-web-devicons.nvim
+
+
 
 echo "Setup complete."
 
--- init.lua
+++ init.lua
@@ -22,38 +22,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 
 
-	-- NOTE: Plugins can also be configured to run lua code when they are loaded.
-	--
-	-- This is often very useful to both group configuration, as well as handle
-	-- lazy loading plugins that don't need to be loaded immediately at startup.
-	--
-	-- For example, in the following configuration, we use:
-	--  event = 'VimEnter'
-	--
-	-- which loads which-key before all the UI elements are loaded. Events can be
-	-- normal autocommands events (`:help autocmd-events`).
-	--
-	-- Then, because we use the `config` key, the configuration only runs
-	-- after the plugin has been loaded:
-	--  config = function() ... end
-
-	{ -- Useful plugin to show you pending keybinds.
-		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
-		config = function() -- This is the function that runs, AFTER loading
-			require("which-key").setup()
-
-			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			})
-		end,
-	},
-
 	-- NOTE: Plugins can specify dependencies.
 	--
 	-- The dependencies are proper plugin specifications as well - anything
@@ -375,25 +343,7 @@ require("lazy").setup({
 		end,
 	},
 
-	{ -- Autoformat
-		"stevearc/conform.nvim",
-		opts = {
-			notify_on_error = false,
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				python = { "isort", "black" },
-				--
-				-- You can use a sub-list to tell conform to run *until* a formatter
-				-- is found.
-				-- javascript = { { "prettierd", "prettier" } },
-			},
-		},
-	},
+
 
 	{ -- Autocompletion
 		"hrsh7th/nvim-cmp",
